391b21420e68abe4b8fd657d8377514fffff542f
Javier Machin <javier_machin_acosta@hotmail.com>
2018-11-20 17:48:51 +0000

    Add API V1 as default, remove empty spec controller files causing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
+
+  # if new versions are added, they would have to be defined above the default version since Rails will cycle 
+  # through all routes from top to bottom searching for one that matches(till method matches? resolves to true).
+
+  # namespace the controllers without affecting th URI; set v1 as default
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
